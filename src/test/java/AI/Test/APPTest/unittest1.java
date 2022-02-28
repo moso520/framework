80a0462c3e89ff1964fdfc927382a0444ca30b35
moso <18302419810@163.com>
2022-02-28 23:50:57 +0800

    jenkins branch

--- src/test/java/AI/Test/APPTest/unittest1.java
+++
@@ -1,8 +0,0 @@
-package AI.Test.APPTest;
-
-public class unittest1 extends unittest{
-    public static void test001(){
-        String name =  new Exception().getStackTrace()[1].getClassName();
-        System.out.println();
-    }
-}
