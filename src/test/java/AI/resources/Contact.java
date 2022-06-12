1fdd7ba0c6cc3a8e5d10ef39e58c1ebe807f3e25
moso <18302419810@163.com>
2022-06-12 22:50:07 +0800

    add sms cases

--- src/test/java/AI/resources/Contact.java
+++ src/test/java/AI/resources/Contact.java
@@ -2,8 +2,8 @@ package AI.resources;
 
 public class Contact {
     //APP server环境账号信息
-    public static final String USER_NAME = "13313778119";
-    public static final String PASSWORD = "7C4A8D09CA3762AF61E59520943DC26494F8941B";
+    public static final String USER_NAME = "19806690967";
+    public static final String PASSWORD = "97BBC79679FE1CFD9AFB52FD6F01D033B479555D";
     public static final String APP_PHONE = "13666666666";
     public static final String SMS_DEFAULT_CODE = "123456";
     public static final String DEVICES_ID = "70f059078a0a97225240a99395adc54";
@@ -12,8 +12,8 @@ public class Contact {
     public static final String MANAGER_ASSWORD = "Aihuandian2021";
     public static final String MANAGER_AUTH = "8888";
     //代理环境账号信息
-    public static final String AGENT_NAME = "13313778119";
-    public static final String AGENT_PASSWORD = "123456";
+    public static final String AGENT_NAME = "19806690967";
+    public static final String AGENT_PASSWORD = "1qazxsw2";
     //逸驰测试环境账号信息
     public static final String TEST_YICHIO_NAME = "john";
     public static final String TEST_YICHIO_PASSWORD = "1234567";
--- src/test/java/AI/resources/InfoApp.java
+++ src/test/java/AI/resources/InfoApp.java
@@ -6,8 +6,6 @@ public class InfoApp {
     private static final String TEST_HOST_2 = "172.20.146.156";
     private static final String TEST_HOST_3 = "172.20.146.170";
     private static final String TEST_HOST_4 = "172.20.146.172";
-    private static final String TEST_HOST_5 = "172.20.146.174";
-    private static final String TEST_HOST_QA = "172.20.146.159";
     public static final String USER_ID = "5c99d9e542cda600684bfc6a";
     /**
      * Host,分别修改后面的数字即可
@@ -15,20 +13,21 @@ public class InfoApp {
      * DEVICES_HOST是devices的HOST
      */
 
-    public static String TEST_HOST = TEST_HOST_3;
-    public static String DEVICES_HOST = TEST_HOST_5;
+    //每台服务器 会有2个appserver 实例， 启动端口分别是8083 ， 8183。
+    public static String TEST_HOST = TEST_HOST_2 + ":8183";
+    public static String DEVICES_HOST = TEST_HOST_3;
 
     //APP
-    public static final String LOGIN = "http://"+ TEST_HOST + ":8083/app/userInfo";
-    public static final String USER_DETAIL = "http://"+ TEST_HOST + ":8083/app/v3/user/userInfo";
-    public static final String UPLOAD_BASIC_INFO = "http://"+ TEST_HOST + ":8083/app/userInfo/basicInfo";
-    public static final String CABINET_LIST = "http://"+ TEST_HOST + ":8083/app/v3/swap/cabinet/list";
-    public static final String GROUP_GRCODE = "http://"+ TEST_HOST + ":8083/app/v3/swap/group/qrcode";
-    public static final String DEPOSIT_AMOUNT = "http://"+ TEST_HOST + ":8083/app/deposit/amount";
-    public static final String SUBMIT_CERTIFICATION = "http://"+ TEST_HOST + ":8083/app/v3/user/userInfo/certification";
-    public static final String SWAP = "http://"+ TEST_HOST + ":8083/app/v3/swap";
-    public static final String SEND_SMS = "http://"+ TEST_HOST_PUB + ":8083/platform/sms";
-    public static final String CHECK_SMS = "http://"+ TEST_HOST_PUB + ":8083/app/v3/login/sms/check";
+    public static final String LOGIN = "http://"+ TEST_HOST + "/app/userInfo";
+    public static final String USER_DETAIL = "http://"+ TEST_HOST + "/app/v3/user/userInfo";
+    public static final String UPLOAD_BASIC_INFO = "http://"+ TEST_HOST + "/app/userInfo/basicInfo";
+    public static final String CABINET_LIST = "http://"+ TEST_HOST + "/app/v3/swap/cabinet/list";
+    public static final String GROUP_GRCODE = "http://"+ TEST_HOST + "/app/v3/swap/group/qrcode";
+    public static final String DEPOSIT_AMOUNT = "http://"+ TEST_HOST + "/app/deposit/amount";
+    public static final String SUBMIT_CERTIFICATION = "http://"+ TEST_HOST + "/app/v3/user/userInfo/certification";
+    public static final String SWAP = "http://"+ TEST_HOST + "/app/v3/swap";
+    public static final String SEND_SMS = "http://"+ TEST_HOST_PUB + "/platform/sms";
+    public static final String CHECK_SMS = "http://"+ TEST_HOST_PUB + ":/app/v3/login/sms/check";
 
     //DEVICES
     public static final String CHECK_DEVICES_VERSION = "http://" + DEVICES_HOST + ":8082/v1/device";
